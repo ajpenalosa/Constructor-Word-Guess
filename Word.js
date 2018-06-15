4a8d585d0605ae85979cfc67a49f4854f446b902
Anthony Penalosa <aj@anthonys-mbp.attlocal.net>
2018-06-14 20:23:12 -0700

    Game is functioning

--- Word.js
+++ Word.js
@@ -4,6 +4,7 @@ var Letter = require("./Letter.js");
 function Word( wordToGuess ) {
     this.wordToGuess = wordToGuess;
     this.wordLetters = [];
+    this.guessedLetters = 0;
 
     for ( var i = 0; i < this.wordToGuess.length; i++ ) {
         var letter = new Letter(this.wordToGuess[i]);
@@ -12,15 +13,23 @@ function Word( wordToGuess ) {
 
     this.wordString = function() {
         this.currentState = "";
+        this.guessedState = "";
         for ( var i = 0; i < this.wordToGuess.length; i++ ) {
-            this.currentState += this.wordLetters[i].displayLetter() + " ";
+            if (this.wordLetters[i].letter === " ") {
+                this.currentState += "   ";
+                this.guessedState += " ";
+            }
+            else {
+                this.currentState += this.wordLetters[i].displayLetter() + " ";
+                this.guessedState += this.wordLetters[i].displayLetter();
+            }
         } 
         console.log(this.currentState);
     }
     
-    this.guessedLetters = function( letterGuessed ) {
-        userGuess( letterGuessed );
-    }
+    // this.guessedLetters = function( letterGuessed ) {
+    //     userGuess( letterGuessed );
+    // }
 }
 
 module.exports = Word;
--- index.js
+++ index.js
@@ -4,11 +4,26 @@ var inquirer = require('inquirer');
 // Import the word constructor
 var Word = require("./Word.js");
 
-var word = new Word("what");
+var newWord = true;
+var guessesRemaining = 10;
+
+// Words
+var words = ["Jurassic World", "Avengers", "Deadpool", "Incredibles"];
+var currentWord = "";
 
 function game() {
 
-    word.wordString();
+    // If this is a new word, then use the Word constructor
+    if ( newWord ) {
+
+        console.log("\r\nGuess the name of the movie\r\n");
+
+        currentWord = new Word(words[Math.floor(Math.random() * words.length)]);
+
+        currentWord.wordString();
+    }
+
+    console.log(" ");
     
     inquirer.prompt([
         {
@@ -17,19 +32,64 @@ function game() {
         }
     ]).then(guess => {
 
-        for ( var i = 0; i < word.wordToGuess.length; i++ ) {
-            if ( word.wordToGuess[i] === guess.letter ) {
-                word.wordLetters[i].userGuess();
-                console.log(word.wordLetters);
+        // Changes the letter guessed to true
+        for ( var i = 0; i < currentWord.wordToGuess.length; i++ ) {
+            if ( currentWord.wordToGuess[i].toLowerCase() === guess.letter.toLowerCase() ) {
+                currentWord.wordLetters[i].userGuess();
             }
         }
+        
+        console.log(" ");
+        currentWord.wordString();
 
-        word.wordString();
+        if ( !currentWord.wordToGuess.toLowerCase().includes(guess.letter.toLowerCase()) && !newWord ) {
+            guessesRemaining--;
+            console.log("\r\n" + guessesRemaining + " guesses remaining!!!");
+        }
+        
+        if ( guessesRemaining === 0 ) {
+            console.log("\r\nGame Over!!\r\n");
+            playAgain();
+        }
 
-        game();
+        if ( currentWord.guessedState === currentWord.wordToGuess ) {
+            console.log("\r\nCONGRATS!! YOU WON!!\r\n");
+            playAgain();
+        }
+        else {
+
+            newWord = false;
+    
+            game();
+
+        }
     
     }); // End of inquirer
 
+}
+
+function playAgain() {
+    
+    inquirer.prompt([
+        {
+            type: "confirm",
+            message: "Would you like to play again?",
+            name: "confirm",
+            default: true
+        }
+    ]).then(answer => {
+
+        if ( answer.confirm ) {
+
+            newWord = true;
+            
+            game();
+        }
+        else {
+            console.log("\r\nThank you for playing! Good bye!\r\n");
+        }
+    
+    }); // End of inquirer
 }
 
 game();
